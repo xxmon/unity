b6dc620dd9835fddeff924f2ca5ba025c824f156
xxmon <mail@simonlindmark.com>
2016-02-22 19:59:35 +0100

    Create SwitchGroup.cs

---
+++ SwitchGroup.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+
+public class SwitchGroup : MonoBehaviour {
+	public GameObject[] switches;
+	public GameObject triggerTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnValidate()
+	{
+		for(int i=0; i < switches.Length; i++)
+		{
+			if (switches [i] == null) {
+				Debug.LogError ("Missing switch.");
+				return;
+			}
+
+			SwitchObject obj = switches [i].GetComponent<SwitchObject>();
+			if (obj == null) {
+				Debug.LogError ("Missing switch.");
+				return;
+			}
+		}
+	}
+
+	bool IsDesiredState()
+	{
+		for(int i=0; i < switches.Length; i++)
+		{
+			if (switches [i] == null) {
+				Debug.LogError ("Missing switch.");
+				return false;
+			}
+
+			SwitchObject obj = switches [i].GetComponent<SwitchObject>();
+			if (obj == null) {
+				Debug.LogError ("Missing switch.");
+				return false;
+			}
+
+			if (obj.desiredState != obj.isOn) {
+				return false;
+			}
+		}
+
+		if(triggerTarget != null)
+		{
+			triggerTarget.SendMessage ("DoActivateTrigger");
+		}
+
+		Debug.Log("IsDesiredState");
+
+		return true;		
+	}
+}
